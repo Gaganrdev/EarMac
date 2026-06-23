163bb8b8cb571ea9802a3e13d907c4d49010ce15
Gagan <gaganrdev@gmail.com>
2026-06-23 15:48:38 +0530

    Initial Commit

---
+++ earmac/earmacApp.swift
@@ -0,0 +1,17 @@
+//
+//  earmacApp.swift
+//  earmac
+//
+//  Created by Gagan R on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct earmacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
